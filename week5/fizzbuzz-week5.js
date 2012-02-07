ba04b9c40802e362d039ba150456124d4f5f4562
jbfink <john.fink@gmail.com>
2012-02-06 19:56:29 -0500

    continue does it. Cheating?

--- week5/fizzbuzz-week5.js
+++ week5/fizzbuzz-week5.js
@@ -2,12 +2,17 @@ for (i=1;i<=20;i++) {
  if (i % 3 === 0) {
  if (i % 5 === 0) {
  console.log("FizzBuzz");
+ continue;
  }
  console.log("Fizz");
+ continue;
 }
 {
 if (i % 5 === 0) {
     console.log("Buzz");
+    continue;
 }
+console.log(i);
+continue;
 }
 }
