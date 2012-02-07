efbf94f697e5165dc03013324c90039492522236
jbfink <john.fink@gmail.com>
2012-02-07 17:04:05 -0500

    Why does the loop only print out the last array value?

---
+++ week5/family.js
@@ -0,0 +1,19 @@
+// Our Person constructor
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var family = [];
+
+// Now we can make an array of people
+family[0] = new Person("alice",40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy", 6);
+
+// loop through our new array
+var i;
+for (i = 0; i < 4; i++) {
+family[i].name;
+}
