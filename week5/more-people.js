40af7d76a9028960ad28b3cdb8199f7d9952b51f
jbfink <john.fink@gmail.com>
2012-02-07 22:23:10 -0500

    more-people.js add.

---
+++ week5/more-people.js
@@ -0,0 +1,14 @@
+Person = function(firstName, lastName, phoneNumber, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+};
+
+var bob = new Person("Bob", "Jones","(650) 777 - 7777","bob.jones@example.com");
+var mary = new Person("Mary", "Johnson","(650) 888 - 8888","mary.johnson@example.com");
+
+var contact = [];
+contact[0] = bob;
+contact[1] = mary;
+
