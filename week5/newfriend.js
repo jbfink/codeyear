a2ea43f07c8ec4a791d965d33f5c44e37b310593
jbfink <john.fink@gmail.com>
2012-02-09 14:15:34 -0500

    Broken newfriend.

---
+++ week5/newfriend.js
@@ -0,0 +1,48 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    
+    phoneNumber: "(650) 777 - 7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    
+    phoneNumber: "(650) 888 - 8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson (person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+list = function () {
+    var length = contacts.length;
+    for (i = 0; i < length; i++) {
+        printPerson(contacts[i]);
+    }
+};
+list();
+
+search = function(lastName) {
+    var length = contacts.length;
+    for (i = 0; i < length; i++) {
+    if (contacts[i].lastName === lastName) {
+        printPerson(contacts[i]);
+    }
+    }
+};
+search("Jones");
+
+add = function(firstName, lastName, email, telephone) {
+    var newFriend = {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.telephone = telephone;
+    }
+};
