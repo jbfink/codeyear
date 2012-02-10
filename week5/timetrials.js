3e2c26fe037dcf3403c7caec566c400f7ec5ab14
jbfink <john.fink@gmail.com>
2012-02-10 14:47:07 -0500

    timetrials.js fixed.

--- week5/timetrials.js
+++ week5/timetrials.js
@@ -5,7 +5,7 @@ var timothy = [12.2,11.8,12.5,10.9,11.1];
 
 // declare your function calculateTotal here
 var calculateTotal = function(raceTimes) {
-    var totalTime;
+    var totalTime = 0;
     for (i = 0; i < raceTimes.length; i++) {
         totalTime = totalTime + raceTimes[i];
     }
