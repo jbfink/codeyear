b090d3e04a7de3ca93054db461905df0e54319e4
jbfink <john.fink@gmail.com>
2012-02-10 14:46:34 -0500

    totaltime busted.

---
+++ week5/timetrials.js
@@ -0,0 +1,20 @@
+// runner times
+var carlos = [9.6,10.6,11.2,10.3,11.5];
+var liu = [10.6,11.2,9.4,12.3,10.1];
+var timothy = [12.2,11.8,12.5,10.9,11.1];
+
+// declare your function calculateTotal here
+var calculateTotal = function(raceTimes) {
+    var totalTime;
+    for (i = 0; i < raceTimes.length; i++) {
+        totalTime = totalTime + raceTimes[i];
+    }
+return totalTime;
+
+};
+
+
+
+var liuTotal = calculateTotal(liu);
+
+console.log(liuTotal);
