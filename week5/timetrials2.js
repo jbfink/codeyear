c5b58910982b90039fe9d3dbb2e812019f6ded61
jbfink <john.fink@gmail.com>
2012-02-10 15:13:31 -0500

    Functioning timetrials2.js. totalTime as a var needed to be declared outside of for loop.

--- week5/timetrials2.js
+++ week5/timetrials2.js
@@ -4,13 +4,15 @@ var sarah = [10.6,11.2,9.4,12.3,10.1];
 var timothy = [12.2,11.8,12.5,10.9,11.1];
 
 var calculateAverage = function (raceTimes) {
+var totalTime = 0;
   for ( i = 0; i < raceTimes.length; i++ ) {
-    var totalTime = (totalTime || 0) + raceTimes[i];
+   totalTime = (totalTime || 0) + raceTimes[i];
   
 // assign variable averageTime
+}
 var averageTime = totalTime / raceTimes.length;    
   return averageTime;
-}
+
 
   };
 
