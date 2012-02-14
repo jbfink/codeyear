bed8850e5ff521f8c3aa11883bd1e163cb4845ab
jbfink <john.fink@gmail.com>
2012-02-14 11:49:39 -0500

    Broken cashregister.3.js

---
+++ week6/cashregister.3.js
@@ -0,0 +1,35 @@
+var cashRegister = {
+    total:0,
+//insert the add method here    
+    add: function(itemCost) {
+    this.total = this.total + this.itemCost
+    }
+    
+    scan: function(item) {
+        switch (item) { 
+        case "eggs": 
+            this.add(0.98); 
+            break;
+        
+        case "milk": 
+            this.add(1.23); 
+            break;
+        
+        //Add other 2 items here
+        case "magazine":
+            this.add(4.99);
+            break;
+            
+        case "chocolate":
+            this.add(0.45);
+            break;
+        
+        }
+        return true;
+    }
+};
+
+//Scan 2 eggs and 3 magazines
+
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
