58e27fde843925301609352013526a3cad5db483
jbfink <john.fink@gmail.com>
2012-02-14 10:54:37 -0500

    Existing, nonfunctional 5.4 (broken by CY I think?

---
+++ week6/5.4.js
@@ -0,0 +1,21 @@
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+  
+   var returnBalance = function() {
+      return bankBalance;
+   };
+       
+   // create the new function here
+   this.askTeller = function() {
+       return returnBalance();
+   };
+}
+
+var john = new Person('John','Smith',30);
+console.log(john.returnBalance);
+var myBalanceMethod = john.askTeller();
+var myBalance = myBalanceMethod();
+console.log(myBalance);
