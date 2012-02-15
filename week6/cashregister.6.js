d9ac2354eb347342952ef4ff9c29aa9b904c8e71
jbfink <john.fink@gmail.com>
2012-02-14 21:01:35 -0500

    Doy. Read instructions and name your things correctly.

--- week6/cashregister.6.js
+++ week6/cashregister.6.js
@@ -1,7 +1,7 @@
 // create a constructor for the StaffMember class
-function StaffMember(name, discount) {
+function StaffMember(name, discountPercent) {
     this.name = name;
-    this.discount = discount;
+    this.discountPercent = discountPercent;
 } 
 
 var sally = new StaffMember("Sally",5);
@@ -10,4 +10,3 @@ var bob = new StaffMember("Bob",10);
 //Create a StaffMember for yourself called me
 
 var me = new StaffMember("John", 20);
-
