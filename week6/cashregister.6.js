4694692045063c6e66fcd8a029141295378062e5
jbfink <john.fink@gmail.com>
2012-02-14 20:55:46 -0500

    Why the hell doesn't this work?

---
+++ week6/cashregister.6.js
@@ -0,0 +1,13 @@
+// create a constructor for the StaffMember class
+function StaffMember(name, discount) {
+    this.name = name;
+    this.discount = discount;
+} 
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+//Create a StaffMember for yourself called me
+
+var me = new StaffMember("John", 20);
+
