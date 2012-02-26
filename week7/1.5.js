405f9e98b5efe4176605c15a5d4511ae42c8d981
jbfink <john.fink@gmail.com>
2012-02-25 19:12:55 -0500

    week6 1.5.js, not working.

---
+++ week7/1.5.js
@@ -0,0 +1,20 @@
+var setName = function(yourName){
+    this.name = "Your name is " + yourName;
+};
+
+// create a human object with the desired method
+var human = {
+  setHumanName: function(name) {
+   setName(name);      
+      
+  }
+};
+
+// now call human.setHumanName
+
+human.setHumanName("Bob");
+console.log(human.name);
+
+// check to see that your method works by printing
+// human.name to the console
+
