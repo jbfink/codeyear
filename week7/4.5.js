257148ed1b272989a074a2c49faf94c4c39e079a
jbfink <john.fink@gmail.com>
2012-02-27 12:20:07 -0500

    I am an idiot.

---
+++ week7/4.5.js
@@ -0,0 +1,18 @@
+// Define substring here
+var substring = function(input, start, end) {
+    var subset = "";
+    var i;
+    return subset;
+};
+var input = "lorem ipsum dolor";
+var start = 6;
+var end = 10;
+var value;
+for (i = start; i<=end; i++) {
+    value += substring[i];
+}
+
+console.log(value);
+// Write test here:
+// Call substring, passing "lorem ipsum dolor" to `input`, `6` to 
+// `start`, and `10` to `end`.
