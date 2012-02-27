332923bfda07cc1530a23a1055a34b0d84181a17
jbfink <john.fink@gmail.com>
2012-02-27 12:05:02 -0500

    Ashamed to admit I cheated a bit on this one.

---
+++ week7/4.4.js
@@ -0,0 +1,9 @@
+// Define substring here
+var substring = function (input,start,end) {
+  var i;
+};
+// Write test here:
+
+var thing = substring("lorem ipsum dolor",6,10);
+// Call substring, passing "lorem ipsum dolor" to `input`, `6` to 
+// `start`, and `10` to `end`.
