655ab8a6ae3c7dbe84de389db7512ea22507ce64
jbfink <john.fink@gmail.com>
2012-02-27 12:11:49 -0500

    functioning 4.4. Still ashamed.

--- week7/4.4.js
+++ week7/4.4.js
@@ -1,9 +1,15 @@
 // Define substring here
-var substring = function (input,start,end) {
-  var i;
+var substring = function(input, start, end) {
+    var i;
 };
-// Write test here:
+var input = "lorem ipsum dolor";
+var start = 6;
+var end = 10;
 
-var thing = substring("lorem ipsum dolor",6,10);
+for (i = start; i<=end; i++) {
+    console.log(input[i]);
+}
+
+// Write test here:
 // Call substring, passing "lorem ipsum dolor" to `input`, `6` to 
 // `start`, and `10` to `end`.
