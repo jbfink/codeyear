2545595f1e1b8653d3a72b10224384d7f56125eb
jbfink <john.fink@gmail.com>
2012-03-01 21:12:49 -0500

    broken 7.4.js. Way too tired right now; I'm stopping.

---
+++ week7/7.4.js
@@ -0,0 +1,18 @@
+
+var r;
+var table = [
+    ["Person",  "Age",  "City"],
+    ["Sue",     22,     "San Francisco"],
+    ["Joe",     45,     "Halifax"]
+];
+var rows = table.length;
+
+for (r = 0; r < rows; r++) {
+ var c;
+ var cells = table[r];
+ var rowText = "";
+ for (c = 0; c < cells; c++) {
+     
+ }
+
+}
